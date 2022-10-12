f0ccd5b592e0f7eb91838c954b209c98e0146491
KaiYuanee <aaa950620@gmail.com>
2022-10-12 20:22:38 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,4 +5,4 @@ bot = commands.Bot(command_prefix='~', intents=intents)
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
-bot.run("MTAyOTcwMDU4MjkyOTM0MjUyNQ.GahXK-.g4h1tsvBK2oRF1Btiqwto0PceCmuMwMxHbWYF8")
+bot.run("MTAyOTcwMDU4MjkyOTM0MjUyNQ.GomMLk.j0yZxbD9Tv_hh2Gcc7ByFS890DzGAFRXd5kxwQ")
